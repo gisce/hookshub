1664e46ca1e626f47c4e38ab2dd3aaf5657cd3f0
Jaume Florez <jflorez@gisce.net>
2016-08-29 11:47:25 +0200

    Defined basic info and moved all tests from status

--- spec/format_githubhook_spec.py
+++ spec/format_githubhook_spec.py
@@ -1,4 +1,5 @@
-from os.path import abspath, normpath, dirname, join
+from os.path import abspath, normpath, dirname, join, isfile
+from os import listdir
 from json import loads
 from hooks.github import GitHubWebhook as github
 from expects import *
@@ -10,19 +11,12 @@ hook_testing = 'github'
 data_path = join(project_path, join('test_data', hook_testing))
 
 with description('Github Hook'):
-    with context('Status event'):
+    with context('Basic info'):
         with it('must have github as origin'):
             file = 'status.json'
             data = open(join(data_path, file)).read()
             hook = github(loads(data))
             expect(hook.origin).to(equal(hook_testing))
-
-        with it('must have status as event'):
-            event = 'status'
-            file = 'status.json'
-            data = open(join(data_path, file)).read()
-            hook = github(loads(data))
-            expect(hook.event).to(equal('status'))
 
         with it('must have project_path/hooks/github as actions_path'):
             file = 'status.json'
@@ -32,21 +26,48 @@ with description('Github Hook'):
                 project_path, join('hooks', hook_testing)
             )))
 
-        with it('must only contain status-powerp-docs within actions'):
+        with it('must contain all actions in actions directory'):
             file = 'status.json'
             data = open(join(data_path, file)).read()
             hook = github(loads(data))
-            expect(len(hook.actions)).to(equal(1))
-            expect(hook.actions[0]).to(equal('status-powerp-docs'))
+            actions = listdir(hook.actions_path)
+            actions = [
+                action for action in actions
+                if isfile(join(hook.actions_path, action))
+                ]
+            expect(hook.actions).to(equal(actions))
 
-        with it('must have status actions only in event_actions, actually 0'):
+        with it('must have all actions of the situation (more details'
+                ' in the readme, event-actions)'):
             file = 'status.json'
             data = open(join(data_path, file)).read()
             hook = github(loads(data))
-            expect(len(hook.event_actions)).to(equal(0))
+            actions = listdir(hook.actions_path)
+            actions = [
+                action for action in actions
+                if isfile(join(hook.actions_path, action))
+                ]
+            actions = [
+                action
+                for action in actions
+                # If they start with {event}-{repository}-{branch}
+                if action.startswith('{0}-{1}-{2}'.format(
+                    hook.event, hook.repo_name, hook.branch_name
+                )) or
+                # If they start with {event}-{repository}_{name}
+                action.startswith(
+                    '{0}-{1}_'.format(hook.event, hook.repo_name)) or
+                # If they are named after {event}-{repository}
+                action == '{0}-{1}'.format(hook.event, hook.repo_name) or
+                # If they start with {event}_{name}
+                action.startswith('{0}_'.format(hook.event)) or
+                # If they are named after {event}
+                action == '{0}'.format(hook.event)
+                ]
+            expect(hook.event_actions).to(equal(actions))
 
         with it('must return the ssh url of the repository'
-                ' (json/repository/ssh_url)'):
+                    ' (json/repository/ssh_url)'):
             file = 'status.json'
             data = open(join(data_path, file)).read()
             json_data = loads(data)
@@ -82,6 +103,24 @@ with description('Github Hook'):
             hook = github(json_data)
             expect(hook.branch_name()).to(equal('None'))
 
+        with it('must return the same params with the get_exe_action and'
+                ' with the get_test_action'):
+            event = 'status'
+            file = 'status.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+            hook.get_exe_action(event)
+            ))
+
+    with context('Status event'):
+        with it('must have status as event'):
+            event = 'status'
+            file = 'status.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('status'))
+
         with it('must return [exe_path, dump(json), event] when getting'
                 ' the execution params for the status event'):
             event = 'status'
@@ -98,13 +137,3 @@ with description('Github Hook'):
             json_data = dumps(dict_json)
             exe_data = [exe_path, json_data, event]
             expect(hook.get_exe_action(event)).to(equal(exe_data))
-
-        with it('must return the same params with the get_exe_action and'
-                ' with the get_test_action'):
-            event = 'status'
-            file = 'status.json'
-            data = open(join(data_path, file)).read()
-            hook = github(loads(data))
-            expect(hook.get_test_action(event)).to(equal(
-                hook.get_exe_action(event)
-            ))
