439950efa2499d2ed9914b6b853f613d29abcb8c
Jaume Florez <jflorez@gisce.net>
2016-08-29 14:06:55 +0200

    Added branch and repo name getters and fixed errors

--- hooks/gitlab.py
+++ hooks/gitlab.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from webhook import webhook
 
+EVENT_COMMENT = 'note'
+EVENT_ISSUE = 'issue'
+EVENT_MERGE_REQ = 'merge_request'
+EVENT_PUSH_TAG = 'tag_push'
+EVENT_PUSH = 'push'
+
 
 class GitLabWebhook(webhook):
 
@@ -9,21 +15,43 @@ class GitLabWebhook(webhook):
         self.origin = 'gitlab'
 
     def ssh_url(self):
-        return self.json['repository']['git_ssh_url'] or 'None'
+        return self.json['repository']['git_ssh_url'] or None
 
     def http_url(self):
-        return self.json['repository']['git_http_url'] or 'None'
+        return self.json['repository']['git_http_url'] or None
 
+    @property
     def event(self):
-        return self.json['object_kind'] or 'None'
+        return self.json['object_kind']
 
+    def repo_name(self):
+        return self.json['repository']['name']
+
+    def branch_name(self):
+        if self.event == EVENT_PUSH:
+            return self.json['ref'].split('/', 2)[-1]
+        elif self.event == EVENT_MERGE_REQ:
+            return self.json['object_attributes']['target_branch']
+        elif self.event == EVENT_ISSUE:
+            return self.json['object_attributes']['branch_name'] or None
+        elif self.event == EVENT_COMMENT:
+            if self.json['issue']:
+                return self.json['issue']['branch_name'] or None
+            elif self.json['merge_request']:
+                return self.json['merge_request']['target_branch'] or None
+            else:
+                None
+        else:
+            return None
+
+    @property
     def event_actions(self):
         # We start with all actions that start with {event}
         # Then we filter them to not execute the actions for the same event
         #  with different repository.
         # Finally we filter what's left to not execute actions with the same
         #  repository but different branches
-        events = super(GitHubWebhook, self).event_actions
+        events = super(GitLabWebhook, self).event_actions
         events = [
             event
             for event in events
