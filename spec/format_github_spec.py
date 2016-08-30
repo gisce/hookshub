ef7219b86b131fc0009b6a114119c9f916d2cf50
Jaume Florez <jflorez@gisce.net>
2016-08-30 09:33:14 +0200

    Added github event issues test

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -113,6 +113,38 @@ with description('Github Hook'):
             hook.get_exe_action(event)
             ))
             
+    with context('Issues event'):
+        with it('must have issues as event'):
+            event = 'issues'
+            file = 'issues.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('issues'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the issues event'):
+            event = 'issues'
+            file = 'issues.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'issues'
+            file = 'issues.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
+            
     with context('Member event'):
         with it('must have member as event'):
             event = 'member'
