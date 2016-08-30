3e689f08edfe02daf11a127c5c4d65ce58bfae32
Jaume Florez <jflorez@gisce.net>
2016-08-30 09:38:10 +0200

    Added test for github fork event

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -113,6 +113,38 @@ with description('Github Hook'):
             hook.get_exe_action(event)
             ))
             
+    with context('Fork event'):
+        with it('must have fork as event'):
+            event = 'fork'
+            file = 'fork.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('fork'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the fork event'):
+            event = 'fork'
+            file = 'fork.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'fork'
+            file = 'fork.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
+            
     with context('Documentation (Gollum) event'):
         with it('must have gollum as event'):
             event = 'gollum'
