d69aea05757d5496c303ed5a17455627773d9004
Jaume Florez <jflorez@gisce.net>
2016-08-30 08:56:48 +0200

    Added test for watch github event

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -271,3 +271,34 @@ with description('Github Hook'):
             expect(hook.get_test_action(event)).to(equal(
                 hook.get_exe_action(event)
             ))
+
+    with context('Watch event'):
+        with it('must have watch as event'):
+            event = 'watch'
+            file = 'watch.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('watch'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the watch event'):
+            event = 'watch'
+            file = 'watch.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'watch'
+            file = 'watch.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
