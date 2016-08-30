7c93c4ec71005782461314573f33b61fe4b8d4d0
Jaume Florez <jflorez@gisce.net>
2016-08-30 10:12:19 +0200

    Fixed context for listener tests

--- spec/format_listener_spec.py
+++ spec/format_listener_spec.py
@@ -8,23 +8,23 @@ project_path = dirname(my_path)              # Project Directory
 data_path = join(project_path, 'test_data')  # Test Directory
 
 with description('Hook Listener'):
-    with it('must return a hook with "webhook" origin if used webhook'
-            ' test_data on instancer method'):
-        webhook_data_path = join(data_path, join('webhook', 'default_event'))
-        webhook_data = loads(open(webhook_data_path, 'r').read())
-        hook = hook_listener.instancer(webhook_data)
-        expect(hook.origin).to(equal('webhook'))
+    with context('Webhook test data'):
+        with it('must return a hook with "webhook" origin on instancer method'):
+            webhook_data_path = join(data_path, join('webhook', 'default_event'))
+            webhook_data = loads(open(webhook_data_path, 'r').read())
+            hook = hook_listener.instancer(webhook_data)
+            expect(hook.origin).to(equal('webhook'))
 
-    with it('must return a hook with "gitlab" origin if used gitlab'
-            ' test_data on instancer method'):
-        webhook_data_path = join(data_path, join('gitlab', 'issue.json'))
-        webhook_data = loads(open(webhook_data_path, 'r').read())
-        hook = hook_listener.instancer(webhook_data)
-        expect(hook.origin).to(equal('gitlab'))
+    with context('GitLab test data'):
+        with it('must return a hook with "GitLab" origin on instancer method'):
+            webhook_data_path = join(data_path, join('gitlab', 'issue.json'))
+            webhook_data = loads(open(webhook_data_path, 'r').read())
+            hook = hook_listener.instancer(webhook_data)
+            expect(hook.origin).to(equal('gitlab'))
 
-    with it('must return a hook with "github" origin if used github'
-            ' test_data on instancer method'):
-        webhook_data_path = join(data_path, join('github', 'status.json'))
-        webhook_data = loads(open(webhook_data_path, 'r').read())
-        hook = hook_listener.instancer(webhook_data)
-        expect(hook.origin).to(equal('github'))
+    with context('GitHub test data'):
+        with it('must return a hook with "GitHub" origin on instancer method'):
+            webhook_data_path = join(data_path, join('github', 'status.json'))
+            webhook_data = loads(open(webhook_data_path, 'r').read())
+            hook = hook_listener.instancer(webhook_data)
+            expect(hook.origin).to(equal('github'))
