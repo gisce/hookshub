529fd591b34d25dcbe021b83cd462f7e87e85fb3
Jaume Florez <jflorez@gisce.net>
2016-08-30 08:55:26 +0200

    Added team_add github event test

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -240,3 +240,34 @@ with description('Github Hook'):
             expect(hook.get_test_action(event)).to(equal(
                 hook.get_exe_action(event)
             ))
+
+    with context('Team Add event'):
+        with it('must have team_add as event'):
+            event = 'team_add'
+            file = 'team_add.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('team_add'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the team_add event'):
+            event = 'team_add'
+            file = 'team_add.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'team_add'
+            file = 'team_add.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
