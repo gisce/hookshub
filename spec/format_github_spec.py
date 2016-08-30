b01a7e8057a51d44a839329ec55c82b17710ab40
Jaume Florez <jflorez@gisce.net>
2016-08-30 09:24:40 +0200

    Added test for github event public

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -112,6 +112,38 @@ with description('Github Hook'):
             expect(hook.get_test_action(event)).to(equal(
             hook.get_exe_action(event)
             ))
+            
+    with context('Public event'):
+        with it('must have public as event'):
+            event = 'public'
+            file = 'public.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('public'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the public event'):
+            event = 'public'
+            file = 'public.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'public'
+            file = 'public.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
 
     with context('Pull Request event'):
         with it('must have pull_request as event'):
