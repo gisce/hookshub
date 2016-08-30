1f0a796f2373a1206c640e98ff3439e638c8566f
Jaume Florez <jflorez@gisce.net>
2016-08-30 09:47:05 +0200

    Added test for github create event

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -113,6 +113,46 @@ with description('Github Hook'):
             hook.get_exe_action(event)
             ))
             
+    with context('Create event'):
+        with it('must have create as event'):
+            event = 'create'
+            file = 'create.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('create'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the create event'):
+            event = 'create'
+            file = 'create.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'create'
+            file = 'create.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
+
+        with it('must return None from payload if ref != branch '
+                '(ref == tag on create.json)'):
+            event = 'create'
+            file = 'create.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.branch_name()).to(equal('None'))
+            
     with context('Delete event'):
         with it('must have delete as event'):
             event = 'delete'
