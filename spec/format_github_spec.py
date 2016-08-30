fc7fa3d4b57dc726cc78082ed93ee0692ac4e5c6
Jaume Florez <jflorez@gisce.net>
2016-08-30 08:50:07 +0200

    Added tests for repository event

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -175,6 +175,37 @@ with description('Github Hook'):
                 hook.get_exe_action(event)
             ))
 
+    with context('Repository event'):
+        with it('must have repository as event'):
+            event = 'repository'
+            file = 'repository.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('repository'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the repository event'):
+            event = 'repository'
+            file = 'repository.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'repository'
+            file = 'repository.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
+
     with context('Status event'):
         with it('must have status as event'):
             event = 'status'
