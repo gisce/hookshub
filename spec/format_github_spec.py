6940e767505c7f00db447b619b047faca51f6659
Jaume Florez <jflorez@gisce.net>
2016-08-30 09:29:14 +0200

    Added membership github event test

--- spec/format_github_spec.py
+++ spec/format_github_spec.py
@@ -113,6 +113,38 @@ with description('Github Hook'):
             hook.get_exe_action(event)
             ))
             
+    with context('Membership event'):
+        with it('must have membership as event'):
+            event = 'membership'
+            file = 'membership.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.event).to(equal('membership'))
+
+        with it('must return [exe_path, dump(json), event] when getting'
+                ' the execution params for the membership event'):
+            event = 'membership'
+            file = 'membership.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            exe_path = join(hook.actions_path, event)
+            from json import dumps
+
+            dict_json = loads(data)
+            json_data = dumps(dict_json)
+            exe_data = [exe_path, json_data, event]
+            expect(hook.get_exe_action(event)).to(equal(exe_data))
+
+        with it('must return the execution params to test the action, if there'
+                ' isn\'t any, it may return the same as the execution params'):
+            event = 'membership'
+            file = 'membership.json'
+            data = open(join(data_path, file)).read()
+            hook = github(loads(data))
+            expect(hook.get_test_action(event)).to(equal(
+                hook.get_exe_action(event)
+            ))
+            
     with context('Page Build event'):
         with it('must have page_build as event'):
             event = 'page_build'
