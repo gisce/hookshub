b26bf1e7bf5fd7cf5c37483a4544667f4a685ba3
Jaume Florez <jflorez@gisce.net>
2016-09-15 12:01:25 +0200

    Added a clarifying tag on action failure logging

--- hookshub/listener.py
+++ hookshub/listener.py
@@ -75,7 +75,7 @@ class HookListener(object):
                     action, stderr
                 ))
                 if proc.returncode != 0:
-                    log += ('[{0}]:{1}Failed!\n'.format(
+                    log += ('[{0}]:{1}\n[{0}]:Failed!\n'.format(
                         action, output
                     ))
                     return -1, log
