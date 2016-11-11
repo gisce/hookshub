c56482d166b2178ac73f625720b800367d95e387
Jaume Florez <jflorez@gisce.net>
2016-11-11 12:41:25 +0100

    Added powerp landing page on comment for PR event

--- hookshub/hooks/github/pull_request-powerp-docs.py
+++ hookshub/hooks/github/pull_request-powerp-docs.py
@@ -148,13 +148,15 @@ with TempDir() as temp:
         branch_name
     )
     if port in ['80', '443']:
-        res_url = '{0}/ca/{1}'.format(base_url, base_uri)
+        res_url = '{0}/'.format(base_url)
+        res_url_ca = '{0}/ca/{1}'.format(base_url, base_uri)
         res_url_es = '{0}/es/{1}'.format(base_url, base_uri)
     else:
-        res_url = '{0}:{1}/ca/{2}'.format(base_url, port, base_uri)
+        res_url = '{0}:{1}/'.format(base_url, port)
+        res_url_ca = '{0}:{1}/ca/{2}'.format(base_url, port, base_uri)
         res_url_es = '{0}:{1}/es/{2}'.format(base_url, port, base_uri)
-    comment = 'Documentation build URL:\n'
-    comment += 'ca_ES: http://{}/\n'.format(res_url)
+    comment = 'Documentation build URL: {}\n'.format(res_url)
+    comment += 'ca_ES: http://{}/\n'.format(res_url_ca)
     comment += 'es_ES: http://{}/'.format(res_url_es)
 
     # Postejem el comentari
