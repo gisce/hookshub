c405353796eaa6c5553730f3a15cd99c874aa81f
Jaume Florez <jflorez@gisce.net>
2017-04-06 15:58:46 +0200

    Remove unused code and unnecessary process
    - Removed build of docs on PR end
    - Removed closed with or without merge check
    - Removed data getters for build
    - Replaced Popen for 'rm' with os.system
    - Removed unused imports
    - Removed unused code in comments

--- hookshub/hooks/github_hooks/pull_request-powerp-docs_endPR.py
+++ hookshub/hooks/github_hooks/pull_request-powerp-docs_endPR.py
@@ -2,29 +2,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-from json import loads, dumps
-from os.path import join
+from json import loads
 from hookshub.hooks.github import GitHubUtil as Util
-from subprocess import Popen, PIPE
 
 import sys
-import tempfile
-import shutil
+from os import system as call
 
 # This script is called whenever a PR is closed. Let it be the cases:
 # - A PR has been merged (action= 'closed', merged= 'True')
 # - A PR has been closed (action= 'closed', merged= 'False')
-
-
-class TempDir(object):
-    def __init__(self):
-        self.dir = tempfile.mkdtemp()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        shutil.rmtree(self.dir)
 
 
 # Carrega dels arguments. Conte els detalls del event
@@ -38,7 +24,6 @@ def arguments():
 payload, event = arguments()
 
 output = ''
-http_url = "https://api.github.com"
 
 action = payload['action']
 if action != Util.actions['ACT_CLOSED']:
@@ -48,109 +33,22 @@ if action != Util.actions['ACT_CLOSED']:
     print (output)
     exit(0)
 
-pr_num = payload['number']
-if not pr_num:
-    output = 'Failed to get number (Is the PR ready?)'
-    print (output)
-    exit(-1)
-
-url = payload['ssh_url'] or payload['http_url']
-if not url:
-    output = 'Failed to get URL (was it in payload?)'
-    print (output)
-    exit(-1)
-
-closed = payload['closed']
 repo_name = payload['repo_name']
-repo_full_name = payload['repo_full_name']
 branch_name = payload['branch_name']
 output += ('Rebut event de <{}> |'.format(event))
-
-# Get from env_vars
-token = payload['token']
-port = payload['port']
 
 
 util_docs_path = '{0}/{1}/powerp_{2}'.format(
     payload['vhost_path'], repo_name, branch_name
 )
 
-docs_path = '{0}/{1}/master'.format(
-    payload['vhost_path'], repo_name
-)
-
-ca_docs_path = '{0}/ca/'.format(
-    docs_path
-)
-es_docs_path = '{0}/es/'.format(
-    docs_path
-)
-
 output = 'Removing data on: {} ...'.format(util_docs_path)
 command = 'rm -r {}'.format(util_docs_path)
 
-delete = Popen(
-    command.split(), stdout=PIPE, stderr=PIPE
-)
-out, err = delete.communicate()
-
-if delete.returncode != 0:
-    output += 'Failed to remove files! {}|'.format(out)
-    output += 'Error on "rm -r": {}'.format(err)
+if call(command) != 0:
+    output += 'Failed to remove files! |'
     print(output)
     exit(-1)
 
 output += 'Removed!|'
-
-# Es pot fer despres de respondre:
-
-# Si s'ha tencat sense mergejar, no actualitzem el build
-#   Altrament fem build de master abans de borrar la PR
-if not closed:
-    with TempDir() as temp:
-        output += ('Creat Directori temporal: {} |'.format(temp.dir))
-
-        # Primer clonem el repositori
-        out, code, err = Util.clone_on_dir(temp.dir, repo_name, url)
-        output += out
-        if code != 0:
-            output += 'Clonant el repository desde http'
-            url = payload['http_url']
-            out, code, err2 = Util.clone_on_dir(temp.dir, repo_name, url)
-            if code != 0:
-                # Could not clone >< => ABORT
-                sys.stderr.write(
-                    '| Failed to get repository {0};\n;{1}|'.format(err, err2)
-                )
-                print(output)
-                exit(-1)
-        output += ' OK |'
-        # Accedim al directori del clone utilitzant el nom del repositori
-
-        clone_dir = join(temp.dir, repo_name)
-
-        # Crear Virtualenv en el directori temporal
-        Util.create_virtualenv(temp.dir, branch_name)
-
-        # Instalem dependencies
-
-        output += Util.pip_requirements(clone_dir)
-
-        # Fem build al directori on tenim la pagina des del directori del clone
-        #   Build en català
-        out, target_build_path = (
-            Util.docs_build(clone_dir, ca_docs_path, None, True)
-        )
-        if not target_build_path:
-            output = '{} FAILED!\n{} |'.format(output, out)
-        output += '{} |'.format(out)
-
-        #   Build en castellà
-        out, target_build_path = (
-            Util.docs_build(clone_dir, es_docs_path, 'mkdocs_es.yml', True)
-        )
-        if not target_build_path:
-            output = '{} FAILED!\n{} |'.format(output, out)
-        output += '{} |'.format(out)
-
 print(output)
