67a2d9e6482db052343c43f2f88a058a77239d02
Jaume Florez <jflorez@gisce.net>
2017-06-26 12:41:46 +0200

    Force pickle to run class and instance methods

--- hookshub/listener.py
+++ hookshub/listener.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from multiprocessing import Pool
-
+from copy_reg import pickle
+from types import MethodType
 import logging
 import signal
 
@@ -14,6 +15,14 @@ from flask import Flask, request, abort, jsonify
 from hookshub.parser import HookParser
 from raven.contrib.flask import Sentry
 
+
+def _pickle_method(m):
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
+
+pickle(MethodType, _pickle_method)
 
 DEFAULT_IP = '0.0.0.0'
 DEFAULT_PORT = 5000
