dfb7b85b03ef0da3b6de9d1f2cbdbef82d4a8a93
Jaume Florez <jflorez@gisce.net>
2017-07-05 12:48:34 +0200

    Add plugins test to update registered plugin

--- spec/format_plugins_spec.py
+++ spec/format_plugins_spec.py
@@ -57,3 +57,39 @@ with description('PluginManager'):
             expect(plugin.repository).to(equal(hook_data.repository))
             expect(plugin.branch).to(equal(hook_data.branch))
 
+        with it('Must update existing hook on PluginManager'):
+            from collections import namedtuple
+            import pudb
+            pu.db
+            with patch("hookshub.plugins.InstanceManager.get") as pm_get:
+                pm_get.start()
+                hook_inst = TestHook()
+                pm_get.return_value = hook_inst
+                # If register is successfull, it'll return the same class
+                expect(plugins.register(TestHook)).to(equal(TestHook))
+                pm_get.stop()
+            # Manual "register" process
+            cls_name = '%s.%s' % (
+                TestHook.__module__, TestHook.__name__
+            )
+            hook_name = cls_name.replace('.', '_')
+            hook_data = namedtuple(
+                hook_name,
+                'name, hook, event, repository, branch'
+            )
+            hook_data.name = hook_name
+            hook_data.hook = hook_inst
+            hook_data.event = hook_inst.event
+            hook_data.repository = hook_inst.repository
+            hook_data.branch = hook_inst.branch
+
+            # Compare manual and PluginManager
+            plugin = plugins.get_hooks()[0]
+            # As we update the plugins, there should not exist any more hooks
+            expect(len(plugins.get_hooks())).to(equal(1))
+            expect(plugin.name).to(equal(hook_data.name))
+            expect(plugin.hook).to(equal(hook_data.hook))
+            expect(plugin.event).to(equal(hook_data.event))
+            expect(plugin.repository).to(equal(hook_data.repository))
+            expect(plugin.branch).to(equal(hook_data.branch))
+
