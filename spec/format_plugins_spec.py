84c6c6f7c5533568e73b474f16cb13820ab47aeb
Jaume Florez <jflorez@gisce.net>
2017-09-05 14:22:26 +0200

    Fix test using "get" method

--- spec/format_plugins_spec.py
+++ spec/format_plugins_spec.py
@@ -59,13 +59,7 @@ with description('PluginManager'):
 
         with it('Must update existing hook on PluginManager'):
             from collections import namedtuple
-            with patch("hookshub.plugins.InstanceManager.get") as pm_get:
-                pm_get.start()
-                hook_inst = TestHook()
-                pm_get.return_value = hook_inst
-                # If register is successfull, it'll return the same class
-                expect(plugins.register(TestHook)).to(equal(TestHook))
-                pm_get.stop()
+            # If register is successfull, it'll return the same class
             # Manual "register" process
             cls_name = '%s.%s' % (
                 TestHook.__module__, TestHook.__name__
@@ -75,16 +69,24 @@ with description('PluginManager'):
                 hook_name,
                 'name, hook, event, repository, branch'
             )
+            hook_inst = TestHook()
             hook_data.name = hook_name
             hook_data.hook = hook_inst
             hook_data.event = hook_inst.event
             hook_data.repository = hook_inst.repository
             hook_data.branch = hook_inst.branch
+            with patch("hookshub.plugins.import_module") as import_method:
+                import_method.start()
+                model = Mock()
+                model.TestHook.return_value = hook_data
+                import_method.return_value = model
+                expect(plugins.register(TestHook)).to(equal(TestHook))
+                import_method.stop()
 
             # Compare manual and PluginManager
-            plugin = plugins.get_hooks()[0]
+            plugin = plugins.get_hooks()[0].hook
             # As we update the plugins, there should not exist any more hooks
-            expect(len(plugins.get_hooks())).to(equal(1))
+            expect(len(plugins)).to(equal(1))
             expect(plugin.name).to(equal(hook_data.name))
             expect(plugin.hook).to(equal(hook_data.hook))
             expect(plugin.event).to(equal(hook_data.event))
