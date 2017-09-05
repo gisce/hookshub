935580a31f8e7ffd107686c7f3e9ebbe89927913
Jaume Florez <jflorez@gisce.net>
2017-09-05 12:00:22 +0200

    Add tests for timeout on running hooks

--- spec/format_parser_spec.py
+++ spec/format_parser_spec.py
@@ -252,6 +252,51 @@ with description('Hook Parser'):
                 get_hook_mock.stop()
 
     with context('Run Hooks (mocked), called from subprocess in production.'):
+        with it('must timeout running all hooks'):
+            with patch('hookshub.hook.get_hooks') as get_hook_mock:
+                with patch('hookshub.parser.logging') as logging:
+                    with patch('hookshub.parser.Pool') as pooler:
+                        webhook_data_path = join(
+                            data_path, join('webhook', 'default_event')
+                        )
+                        default_conf = {
+                            'github_token': 'GHT',
+                            'gitlab_token': 'GLT',
+                            'vhost_path': 'VHP'
+                        }
+                        proc = Mock()
+                        proc.wait.return_value = True
+                        proc.ready.return_value = False
+
+                        pool = Mock()
+                        pool.apply_async.return_value = proc
+
+                        pooler.start()
+                        pooler.return_value = pool
+
+                        logging.start()
+                        logging.basicConfig.return_value = True
+                        logging.info = True
+                        logger = Mock()
+                        logger.info.return_value = True
+                        logger.error.return_value = True
+                        logging.getLogger.return_value = logger
+
+                        hook_used = Mock()
+                        hook_used.get_args.return_value = {}
+                        hook_used.run_hook = lambda *a: True
+
+                        get_hook_mock.start()
+                        get_hook_list = [('hook_name', hook_used)]
+                        get_hook_mock.return_value = get_hook_list
+
+                        parser = HookParser(webhook_data_path, 'default_event',
+                                            pool=pooler())
+                        res = parser.run_event_hooks(def_conf=default_conf)
+                        expected_code = 0
+                        expected_log = '[hook_name]:Success!\n'
+                        expect(res).to(equal((expected_code, expected_log)))
+
         with it('must run successfully all hooks'):
             with patch('hookshub.hook.get_hooks') as get_hook_mock:
                 with patch('hookshub.parser.logging') as logging:
