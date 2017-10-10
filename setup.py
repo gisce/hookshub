1d05c02d7a5cb205b033dd8aac34b978550fa671
Jaume Florez <jflorez@gisce.net>
2017-10-10 14:58:41 +0200

    Add requriements to install_requires on setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup
 
+requirements = [
+      'requests',
+      'osconf',
+      'flask',
+      'raven==5.0.0',
+      'blinker',
+      'virtualenv'
+]
+
 setup(name='hookshub',
       version='1.0',
       description='A module for parsing and acting against JSON hooks',
@@ -7,6 +16,7 @@ setup(name='hookshub',
       author='Jaume Florez',
       author_email='jflorez@gisce.net',
       license='MIT',
+      install_requires=requirements,
       packages=[
           'hookshub', 'hookshub/hooks', 'hookshub/hooks/webhook_hooks/',
       ],
